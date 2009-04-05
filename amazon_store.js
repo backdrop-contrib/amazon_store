3549c43e614133ce16a040969a8c2b729e548b40
Randy Fay <rfay@30906.no-reply.drupal.org>
2009-04-05 14:51:01 +0000

    Added amazon_store.js and CHANGELOG.txt. Fixed bogus menu items under amazon_store

---
+++ amazon_store.js
@@ -0,0 +1,8 @@
+// $Id$
+
+Drupal.behaviors.amazonStoreShowHide = function () {
+  $(".togglebtn").click(function () {
+    // Looks down from 2 levels up for an element with class=toggle
+    $(this).parent().parent().find('.toggle').toggle();
+  });
+}
