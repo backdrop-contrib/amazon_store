bd89dcfce3bdd3655a521486e049aed9a67211a8
Randy Fay <rfay@30906.no-reply.drupal.org>
2009-04-28 22:12:39 +0000

    Adding 2 files that got left out

---
+++ amazon_store_search_block.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * $Id$
+ * @file
+ * 	Contents of amazon_store_search_block
+ */
+print drupal_get_form('amazon_store_search_form');
+?>
+<div class="cart-button"><a href="<?php print url("amazon_store/cart") ?>" title="View your shopping cart">
+<img src="<?php print url("$directory/images/shopping_cart.png") ?>" /></a>
+</div>
