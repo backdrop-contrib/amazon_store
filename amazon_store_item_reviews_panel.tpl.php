1e45ffe9fc93fe8170d3bb9e3b801137eb2fd586
Randy Fay <rfay@30906.no-reply.drupal.org>
2010-12-01 23:26:50 +0000

    #905200 by rfay: Remove/adjust customer reviews

--- amazon_store_item_reviews_panel.tpl.php
+++ amazon_store_item_reviews_panel.tpl.php
@@ -5,28 +5,10 @@
  *   template for item_reviews panel
  */
 
-if ($item->CustomerReviews->Review) {
-  $rounded_avg = round($item->CustomerReviews->AverageRating);
-
+if (!empty($item->CustomerReviews->IFrameURL)) {
 ?>
-<p><strong>Average Amazon User Rating:</strong>
-  <?php print theme('image', array('path' => "$directory/images/{$rounded_avg}stars.gif", 'title' => "{$item->CustomerReviews->AverageRating} stars", 'alt' => "{$item->CustomerReviews->AverageRating} stars")); ?>
-
-<?php
-  foreach ($item->CustomerReviews->Review as $review) {
-
-    $rounded_stars = round($review->Rating);
-    ?>
 <div class="customer_review">
-<p>
-  <?php print theme('image', array('path' => "$directory/images/{$rounded_stars}stars.gif", 'title' => "{$review->Rating} stars", 'alt' => "{$review->Rating} stars")); ?>
-	<strong> <?php print $review->Summary ?></strong>
-	<?php print $review->Date ?></p>
-<p>Reviewer: <strong><?php print $review->Reviewer->Name ?></strong></p>
-
-<p><?php print $review->Content ?></p>
+<iframe class="customer_review_iframe" src="<?php print check_url($item->CustomerReviews->IFrameURL);?>"/>
 </div>
 <?php
-
-  }
 }
