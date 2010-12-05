a991d7857d479f052a156952d17c7ef5781808c3
Randy Fay <rfay@30906.no-reply.drupal.org>
2010-12-05 19:02:25 +0000

    #989106 by rfay: Fix show/hide javascript

--- amazon_store.js
+++ amazon_store.js
@@ -1,8 +1,13 @@
 // $Id$
 
-Drupal.behaviors.amazonStoreShowHide = function () {
-  $(".togglebtn").click(function () {
-    // Looks down from 2 levels up for an element with class=toggle
-    $(this).parent().parent().find('.toggle').toggle();
-  });
-}
+(function ($) {
+
+  Drupal.behaviors.exampleModule = {
+    attach: function(context, settings) {
+      $('.togglebtn', context).click(function () {
+        $(this).parent().find('.toggle').toggle();
+      });
+    }
+  };
+
+})(jQuery);
