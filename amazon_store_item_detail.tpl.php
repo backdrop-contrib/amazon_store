bd625c7739a896a0c1ed7322de99dcc61d93fade
Randy Fay <rfay@30906.no-reply.drupal.org>
2011-01-29 22:01:08 +0000

    #1010484 by the_g_bomb, rfay: Fix markup in amazon_store_item_detail.tpl.php

--- amazon_store_item_detail.tpl.php
+++ amazon_store_item_detail.tpl.php
@@ -6,46 +6,44 @@
  */
 ?>
 
-<div id="main-wrapper" class="item-detail-wrapper">
-<div class="columns-wrapper">
-<?php drupal_set_title((string)$amazon_item->ItemAttributes->Title); ?>
-<!--  Top section, with title and navigation, cart -->
-<div id="topnav">
-<h3 id="title"><?php print (string)$amazon_item->ItemAttributes->Title ?></h3>
+<div id="main-item-wrapper" class="item-detail-wrapper">
+  <div class="columns-wrapper">
+    <?php drupal_set_title((string)$amazon_item->ItemAttributes->Title); ?>
+    <!--  Top section, with title and navigation, cart -->
+    <div id="topnav">
+      <h3 id="title"><?php print (string)$amazon_item->ItemAttributes->Title ?></h3>
+      <a href="<?php print url("amazon_store") ?>">Continue Shopping</a> or <a href='<?php print url("amazon_store/cart")?>'>See your cart</a>
+    </div>
+    <!-- Left section: Item Description and Product Reviews -->
+    <div id="item-details" class="left-column column">
+      <h3>Item Description</h3>
+      <p id="product-description">
+        <?php
+        if (!empty($editorialreview)) {
+          print $editorialreview;
+        }
+        ?>
+      </p>
 
-<a href="<?php print url("amazon_store") ?>">Continue Shopping</a> or <a
-  href='<?php print url("amazon_store/cart")?>'>See your cart</a></div>
+      <h3>Product Details</h3>
+      <div id="product-details">
+        <?php print theme('amazon_store_details_panel', array('item' => $amazon_item)); ?>
+      </div>
+    </div> <!-- End item-details -->
 
-<!-- Left section: Item Description and Product Reviews -->
-<div id="item-details" class="left-column column">
-<h3>Item Description</h3>
-<p id="product-description">
-<?php
-if (!empty($editorialreview)) {
-  print $editorialreview;
-}
-?>
-</p>
+    <div id="right-column" class="column">
+      <?php print theme('amazon_store_item_image', array('amazon_item' => $amazon_item, 'size' => 'MediumImage')); ?>
 
-<h3>Product Details</h3>
-<p id="product-details"><?php print theme('amazon_store_details_panel', array('item' => $amazon_item)); ?>
-</p>
-</div>
-<!-- End item-details -->
+      <h3>Buying Options</h3>
+      <?php print theme('amazon_store_item_offers', array('amazon_item' => $amazon_item)); ?>
 
-<div id="right-column" class="column">
-<?php print theme('amazon_store_item_image', array('amazon_item' => $amazon_item, 'size' => 'MediumImage')); ?>
+      <h3>Similar Items</h3>
+      <?php print theme('amazon_store_similar_items_panel', array('item' => $amazon_item)); ?>
+    </div> <!-- end right-column -->
 
-<h3>Buying Options</h3>
-<?php print theme('amazon_store_item_offers', array('amazon_item' => $amazon_item)); ?>
-
-<h3>Similar Items</h3>
-<?php print theme('amazon_store_similar_items_panel', array('item' => $amazon_item)); ?>
-</div>
-
-</div>
-<div id="bottom-section" class="column">
-<h3>Customer Reviews</h3>
-<?php print theme('amazon_store_item_reviews_panel', array('item' => $amazon_item)); ?></div>
-
+  </div> <!-- end columns-wrapper -->
+  <div id="bottom-section" class="column">
+    <h3>Customer Reviews</h3>
+    <?php print theme('amazon_store_item_reviews_panel', array('item' => $amazon_item)); ?>
+  </div> <!-- end bottom-section -->
 </div>
