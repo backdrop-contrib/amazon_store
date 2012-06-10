04ef7d272cbc0f78cf64116e4d3643b0537526ff
Randy Fay <randy@randyfay.com>
2012-06-10 17:20:00 -0600

    Issue #1492124 by kedia.us: Fixed Use of <iframe ... /> breaks code in chrome.

--- amazon_store_item_reviews_panel.tpl.php
+++ amazon_store_item_reviews_panel.tpl.php
@@ -3,11 +3,10 @@
  * @file
  *   template for item_reviews panel
  */
-
-if (!empty($item->CustomerReviews->IFrameURL)) {
 ?>
-<div class="customer_review">
-<iframe class="customer_review_iframe" src="<?php print check_url($item->CustomerReviews->IFrameURL);?>"/>
-</div>
-<?php
-}
+
+<?php if (!empty($item->CustomerReviews->IFrameURL)): ?>
+  <div class="customer_review">
+    <iframe class="customer_review_iframe" src="<?php print check_url($item->CustomerReviews->IFrameURL);?>"></iframe>
+  </div>
+<?php endif; ?>
